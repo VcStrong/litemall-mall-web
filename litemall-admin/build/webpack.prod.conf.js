1b46d453c42eca68468d20a54b39851ae5aef772
VcStrong <953022119@qq.com>
2019-08-21 16:34:50 +0800

    添加

--- litemall-admin/build/webpack.prod.conf.js
+++ litemall-admin/build/webpack.prod.conf.js
@@ -31,6 +31,16 @@ const webpackConfig = merge(baseWebpackConfig, {
       usePostCSS: true
     })
   },
+  performance: {
+    hints: "warning", // 枚举
+    maxAssetSize: 30000000, // 整数类型（以字节为单位）
+    maxEntrypointSize: 50000000, // 整数类型（以字节为单位）
+    assetFilter: function(assetFilename) {
+      // 提供资源文件名的断言函数
+      return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+
+    }
+  },
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
--- litemall-admin/config/dev.env.js
+++ litemall-admin/config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
 	NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"http://localhost:9080/admin"'
+  BASE_API: '"http://106.13.91.63:9080/admin"'
 }
